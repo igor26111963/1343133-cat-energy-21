78be9bc362ff5328320e5fa34da3eb307c8d6660
igor26111963 <poltorak63@mail.ru>
2021-01-25 07:50:35 +0300

    убрал скрол

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ const styles = () => {
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(less())
+    .pipe(gulp.dest("build/css"))
     .pipe(postcss([
       autoprefixer(),
       csso()
